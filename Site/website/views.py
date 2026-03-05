4bbc2821ff1ff12f87823568a1e7b28fca7c3e71
unknown <erikmoreirabarbato@gmail.com>
2026-03-05 09:20:03 -0300

    Criação de urls e links

--- Site/website/views.py
+++ Site/website/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 class Index(TemplateView):
     template_name = "website/minhahome.html"
 
+class Sobre(TemplateView):
+    template_name = "website/sobre.html"
+
+class Contato(TemplateView):
+    template_name = "website/contato.html"
+
