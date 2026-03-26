9fc5e4932245410c345b6544c7ca94775fdf72cc
unknown <erikmoreirabarbato@gmail.com>
2026-03-26 09:21:12 -0300

    Criação de models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Tema(models.Model):
+    nome = models.CharField(max_length=255)
+    cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+class Subtema(models.Model):
+    nome = models.CharField(max_length=255)
+    tema = models.ForeignKey(Tema, on_delete=models.CASCADE, related_name='subtemas')
+    cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+class Video(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    link = models.URLField()
+    ativo = models.BooleanField(default=True)
+    subtema = models.ForeignKey(Subtema, on_delete=models.CASCADE, related_name='videos')
+    cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+class Comentario(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.CharField(max_length=200)
+    cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+class Avaliacao(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='avaliacoes')
+    nota = models.PositiveSmallIntegerField()
+    cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    comentario = models.ForeignKey(Comentario, on_delete=models.CASCADE, related_name='likes')
+    like = models.BooleanField()
+    cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+
+    
