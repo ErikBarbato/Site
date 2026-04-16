6413cd649125a0f486e88e54afacfb5d05dfd31c
unknown <erikmoreirabarbato@gmail.com>
2026-04-16 09:25:08 -0300

    Criação do menu

--- website/models.py
+++ website/models.py
@@ -6,11 +6,17 @@ class Tema(models.Model):
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.nome}"
+
 class Subtema(models.Model):
     nome = models.CharField(max_length=255)
     tema = models.ForeignKey(Tema, on_delete=models.CASCADE, related_name='subtemas')
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nome}"
 
 class Video(models.Model):
     titulo = models.CharField(max_length=255)
@@ -21,23 +27,34 @@ class Video(models.Model):
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.titulo}"
+
 class Comentario(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='comentarios')
     texto = models.CharField(max_length=200)
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.texto}"
+
 class Avaliacao(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='avaliacoes')
     nota = models.PositiveSmallIntegerField()
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.nota}"
 
 class Like(models.Model):
     comentario = models.ForeignKey(Comentario, on_delete=models.CASCADE, related_name='likes')
     like = models.BooleanField()
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
     cadastrado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.like}"
 
 
     
--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("inicio", Index.as_view(), name="pagina_inicial"),
     path("sobre", Sobre.as_view(), name="sobre"),
     path("contato", Contato.as_view(), name="contato"),
+    
 
 
     #urls 
